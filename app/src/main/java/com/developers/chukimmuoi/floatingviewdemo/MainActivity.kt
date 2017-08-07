745ec20bec0fa3bb6d227bb2fee837ebb051c31d
chukimmuoi <chukimmuoi@gmail.com>
2017-08-07 22:32:12 +0700

    Add code window

--- app/src/main/java/com/developers/chukimmuoi/floatingviewdemo/MainActivity.kt
+++ app/src/main/java/com/developers/chukimmuoi/floatingviewdemo/MainActivity.kt
@@ -1,12 +1,54 @@
 package com.developers.chukimmuoi.floatingviewdemo
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.developers.chukimmuoi.floatingviewdemo.service.FloatingService
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = MainActivity::class.java.simpleName
+
+    private val CODE_DRAW_OVER_OTHER_APP_PERMISSION: Int = 2048
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + packageName))
+            startActivityForResult(intent, CODE_DRAW_OVER_OTHER_APP_PERMISSION)
+        } else {
+            initializeView()
+        }
+    }
+
+    fun initializeView() {
+        mActivityMainFloatingButton.setOnClickListener {
+            startService(Intent(this@MainActivity, FloatingService::class.java))
+            finish()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == CODE_DRAW_OVER_OTHER_APP_PERMISSION) {
+
+            //Check if the permission is granted or not.
+            if (resultCode == RESULT_OK) {
+                initializeView();
+            } else { //Permission is not available
+                Toast.makeText(this, "Draw over other app permission not available. Closing the application", Toast.LENGTH_SHORT).show();
+
+                finish();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
---
+++ app/src/main/java/com/developers/chukimmuoi/floatingviewdemo/service/FloatingService.kt
@@ -0,0 +1,158 @@
+package com.developers.chukimmuoi.floatingviewdemo.service
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.graphics.PixelFormat
+import android.os.IBinder
+import android.view.*
+import com.developers.chukimmuoi.floatingviewdemo.MainActivity
+import com.developers.chukimmuoi.floatingviewdemo.R
+import kotlinx.android.synthetic.main.item_collapsed.view.*
+import kotlinx.android.synthetic.main.item_expanded.view.*
+
+
+/**
+ * @author  : Hanet Electronics
+ * @Skype   : chukimmuoi
+ * @Mobile  : +84 167 367 2505
+ * @Email   : muoick@hanet.com
+ * @Website : http://hanet.com/
+ * @Project : FloatingViewDemo
+ * Created by chukimmuoi on 04/08/2017.
+ */
+
+/**
+ * STARTED: Một service được gọi là STARTED khi một thành phần của ứng dụng,
+ *          chẳng hạn như là activity, start nó bằng cách gọi phương thức startService().
+ *          Mỗi lần được started, service chạy bên dưới vô thời hạn,
+ *          thậm chí ngay cả khi thành phần đã started nó bị hủy.
+ *          StartService --> onCreate() --> onStartCommand --> (Run) --> onDestroy --> ServiceShutDown.
+ *
+ * BOUND  : Một service được gọi là bound khi một thành phần ứng dụng
+ *          liên kết với nó bằng cách gọi phương thức bindService().
+ *          Một dịch vụ ràng buộc cung cấp một giao diện client-server
+ *          cho phép các thành phần tương tác với service, gửi yêu cầu, nhận kết quả,
+ *          thậm chí tương tự trong việc giao tiếp với interprocess (IPC).
+ *          BindService --> onCreate --> onBind --> (Run) --> onUnbind() <-> onRebind --> onDestroy --> ServiceShutDown.
+ *
+ * Stop service: call stopShell() or stopService()
+ *
+ * 2 loại Service: Local Service & Remote Service*/
+
+class FloatingService : Service() {
+
+    private val TAG = FloatingService::class.java.simpleName
+
+    private lateinit var mWindowManager: WindowManager
+
+    private lateinit var mFloatingView: View
+
+    override fun onCreate() {
+        super.onCreate()
+
+        mWindowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+
+        mFloatingView  = LayoutInflater.from(this).inflate(R.layout.layout_floating, null)
+
+        val params = WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.TYPE_PHONE, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, PixelFormat.TRANSLUCENT)
+
+        params.gravity = Gravity.TOP or Gravity.LEFT
+        params.x = 0
+        params.y = 100
+
+        mWindowManager.addView(mFloatingView, params)
+
+        mFloatingView.mCollapsedImgClose.setOnClickListener { stopSelf() }
+
+        mFloatingView.mExpandedImgClose.setOnClickListener {
+            mFloatingView.mCollapsedView.visibility = View.VISIBLE
+            mFloatingView.mExpandedView.visibility = View.GONE
+        }
+
+        mFloatingView.mExpandedImgOpen.setOnClickListener {
+            val intent = Intent(this@FloatingService, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+
+            stopSelf()
+        }
+
+        mFloatingView.setOnTouchListener(object : View.OnTouchListener {
+            private var initialX: Int = 0
+            private var initialY: Int = 0
+            private var initialTouchX: Float = 0.toFloat()
+            private var initialTouchY: Float = 0.toFloat()
+
+            override fun onTouch(view: View, event: MotionEvent): Boolean {
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        initialX = params.x
+                        initialY = params.y
+
+                        initialTouchX = event.rawX
+                        initialTouchY = event.rawY
+
+                        return true
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        val XDiff = (event.rawX - initialTouchX).toInt()
+                        val YDiff = (event.rawY - initialTouchY).toInt()
+
+                        if (XDiff < 10 && YDiff < 10) {
+                            if (isViewCollapsed()) {
+                                mFloatingView.mCollapsedView.visibility = View.GONE
+                                mFloatingView.mExpandedView.visibility = View.VISIBLE
+                            }
+                        }
+
+                        return true
+                    }
+                    MotionEvent.ACTION_MOVE -> {
+                        params.x = initialX + (event.rawX - initialTouchX).toInt()
+                        params.y = initialY + (event.rawY - initialTouchY).toInt()
+
+                        mWindowManager.updateViewLayout(mFloatingView, params)
+
+                        return true
+                    }
+                }
+                return false
+            }
+        })
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        // Tạo lại các dịch vụ sau khi đã có đủ bộ nhớ
+        // Và gọi onStartCommand() một lần nữa với một Intent null.
+        return Service.START_STICKY
+
+        // Không bận tâm tái tạo các dịch vụ một lần nữa.
+        //return Service.START_NOT_STICKY
+
+        // Tạo lại các dịch vụ
+        // Và truyền một Intent tương tự cho onStartCommand().
+        //return Service.START_REDELIVER_INTENT
+    }
+
+    override fun onBind(intent: Intent?): IBinder {
+        TODO("not implemented")
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        return super.onUnbind(intent)
+    }
+
+    override fun onRebind(intent: Intent?) {
+        super.onRebind(intent)
+    }
+
+    fun isViewCollapsed(): Boolean {
+        return mFloatingView != null || mFloatingView.mCollapsedView.visibility == View.VISIBLE
+    }
+
+    override fun onDestroy() {
+        mFloatingView?.let { mWindowManager.removeView(mFloatingView) }
+        super.onDestroy()
+    }
+}
